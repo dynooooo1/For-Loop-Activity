6cc5d6ed0510fe79c0918692b16f49a554ebcaec
Dylan Dominno <dylandominno7@gmail.com>
2024-10-10 21:08:36 +0800

    evenNumbers.js

---
+++ evenNumbers.js
@@ -0,0 +1,14 @@
+// evenNumbers.js
+
+// Print numbers from 1 to 10 using a for loop
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+}
+
+// Modify the program to print only even numbers between 1 and 10
+console.log("Even numbers between 1 and 10:");
+for (let i = 1; i <= 10; i++) {
+    if (i % 2 === 0) {
+        console.log(i);
+    }
+}
